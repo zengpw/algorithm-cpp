ecd41e840d86af9161b05020ede20e1693f8ee6d
Vincent Zeng <vincent.zeng@blackboard.com>
2018-11-23 16:38:52 +0800

    add header files

---
+++ include/basic/recurrence.h
@@ -0,0 +1,14 @@
+//
+//  recurrence.h
+//  algorithm-cpp
+//
+//  Created by Vincent Zeng on 2018/11/20.
+//  Copyright © 2018 Vincent Zeng. All rights reserved.
+//
+
+#ifndef basic_recurrence_h
+#define basic_recurrence_h
+
+#include "include/util/intUtil.h"
+
+#endif // basic_recurrence_h
---
+++ include/basic/recursion.h
@@ -0,0 +1,14 @@
+//
+//  recursion.h
+//  algorithm-cpp
+//
+//  Created by Vincent Zeng on 2018/11/20.
+//  Copyright © 2018 Vincent Zeng. All rights reserved.
+//
+
+#ifndef basic_recursion_h
+#define basic_recursion_h
+
+#include "include/util/intUtil.h"
+
+#endif // basic_recursion_h
